7a10fa4043a0582c4b0ccc5f23b580d233d7e282
rich.quackenbush <rich.quackenbush@captiveaire.com>
2015-04-13 09:40:41 -0400

    Initial commit.

---
+++ Cas.Common.WPF/Controls/BindablePasswordBox.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cas.Common.WPF.Controls
+{
+    /// <summary>
+    /// Provides a pasword box that can be bound to using MVVM.
+    /// </summary>
+    public class BindablePasswordBox : Decorator
+    {
+        /// <summary>
+        /// Password Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty PasswordProperty
+            = DependencyProperty.Register("Password", typeof (string), typeof (BindablePasswordBox),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    OnDependencyPropertyChanged));
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        private static void OnDependencyPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            var p = source as BindablePasswordBox;
+            if (p != null)
+            {
+                if (e.Property == PasswordProperty)
+                {
+                    var pb = p.Child as PasswordBox;
+                    if (pb != null)
+                    {
+                        if (pb.Password != p.Password)
+                            pb.Password = p.Password;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates an instance of of the BindablePasswordBox.
+        /// </summary>
+        public BindablePasswordBox()
+        {
+            Child = new PasswordBox();
+            ((PasswordBox)Child).PasswordChanged += BindablePasswordBox_PasswordChanged;
+            this.Focusable = true;
+        }
+
+        void BindablePasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = ((PasswordBox)Child).Password;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            Child.Focus();
+        }
+    }
+}
---
+++ Cas.Common.WPF/ExtensionProperties/FocusExtension.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+
+namespace Cas.Common.WPF.ExtensionProperties
+{
+    /// <summary>
+    /// Provides a way to set focus via binding from a ViewModel.
+    /// </summary>
+    public static class FocusExtension
+    {
+        /// <summary>
+        /// Gets the value of the IsFocused property.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool GetIsFocused(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsFocusedProperty);
+        }
+
+        /// <summary>
+        /// Sets the value of the IsFocused property.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetIsFocused(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFocusedProperty, value);
+        }
+
+        /// <summary>
+        /// Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached(
+             "IsFocused", typeof(bool), typeof(FocusExtension),
+             new UIPropertyMetadata(false, OnIsFocusedPropertyChanged));
+
+
+        private static void OnIsFocusedPropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var uie = (UIElement)d;
+            if ((bool)e.NewValue)
+            {
+                uie.Focus(); // Don't care about false values.
+            }
+        }
+    }
+}
