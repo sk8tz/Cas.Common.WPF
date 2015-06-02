51d12cdb10058efc19d58c4a0d2b136e778d5614
rich.quackenbush <rich.quackenbush@captiveaire.com>
2015-06-02 16:24:44 -0400

    Added InvertBooleanConverter

---
+++ Cas.Common.WPF/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Cas.Common.WPF.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool) value;
+        }
+    }
+}
