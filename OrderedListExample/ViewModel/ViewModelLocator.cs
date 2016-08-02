7686d167c24c4445aa165cadfd9561300d292305
rich.quackenbush <rich.quackenbush@captiveaire.com>
2016-08-02 15:23:09 -0400

    Deletion can now be cancelled in the ordered list view model.

--- OrderedListExample/ViewModel/ViewModelLocator.cs
+++ OrderedListExample/ViewModel/ViewModelLocator.cs
@@ -1,10 +1,12 @@
-﻿namespace OrderedListExample.ViewModel
+﻿using Cas.Common.WPF;
+
+namespace OrderedListExample.ViewModel
 {
     public static class ViewModelLocator
     {
         public static MainViewModel Main
         {
-            get {  return new MainViewModel(); }
+            get {  return new MainViewModel(new MessageBoxService()); }
         }
     }
 }
